4ce1e584d6d06086f56da145c83964ff6f405b35
Francisco <javiermartincuenca@gmail.com>
2024-05-14 17:55:54 +0200

    Update license

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,7 @@ import { component$ } from '@builder.io/qwik';
 export default component$(() => {
   return (
     <main>
-      <h1>Hello world</h1>
+      <h1>Wongames Qwik</h1>
     </main>
   );
 });
