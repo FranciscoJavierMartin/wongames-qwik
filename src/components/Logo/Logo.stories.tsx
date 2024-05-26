414f638173c1f933692f4318b68fca6a4b8e1a8c
Francisco <javiermartincuenca@gmail.com>
2024-05-26 10:32:23 +0200

    Add stories

--- src/components/Logo/Logo.stories.tsx
+++ src/components/Logo/Logo.stories.tsx
@@ -14,3 +14,49 @@ export default meta;
 export const Default: StoryObj<LogoProps> = {
   render: (props) => <Logo {...props} />,
 };
+
+export const Black: StoryObj<LogoProps> = {
+  args: {
+    color: 'black',
+  },
+  render: (props) => <Logo {...props} />,
+};
+
+export const White: StoryObj<LogoProps> = {
+  args: {
+    color: 'white',
+  },
+  render: (props) => <Logo {...props} />,
+};
+
+export const Normal: StoryObj<LogoProps> = {
+  args: {
+    color: 'black',
+    size: 'normal',
+  },
+  render: (props) => <Logo {...props} />,
+};
+
+export const Large: StoryObj<LogoProps> = {
+  args: {
+    color: 'black',
+    size: 'large',
+  },
+  render: (props) => <Logo {...props} />,
+};
+
+export const ShowOnMobile: StoryObj<LogoProps> = {
+  args: {
+    color: 'black',
+    hideOnMobile: false,
+  },
+  render: (props) => <Logo {...props} />,
+};
+
+export const HideOnMobile: StoryObj<LogoProps> = {
+  args: {
+    color: 'black',
+    hideOnMobile: true,
+  },
+  render: (props) => <Logo {...props} />,
+};
