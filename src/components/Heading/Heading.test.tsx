11096d580442cb39beba2ac39a0cdd84c40fdd64
Francisco <javiermartincuenca@gmail.com>
2024-06-01 18:48:41 +0200

    First test

--- src/components/Heading/Heading.test.tsx
+++ src/components/Heading/Heading.test.tsx
@@ -2,12 +2,19 @@ import { createDOM } from '@builder.io/qwik/testing';
 import Heading from '.';
 
 describe('<Heading/>', () => {
-  it('should render the heading', async () => {
+  it('should render a white heading by default', async () => {
     const { screen, render } = await createDOM();
-    await render(<Heading />);
+    await render(<Heading>Most populars</Heading>);
 
-    expect(screen.getElementsByTagName('h1').item(0)?.innerHTML).toMatch(
-      /wongames/i,
+    expect(screen.getElementsByTagName('h2').item(0)?.innerHTML).toMatch(
+      /Most populars/i,
     );
+
+    expect(
+      screen
+        .getElementsByTagName('h2')
+        .item(0)
+        ?.classList.contains('text-white'),
+    ).toBe(true);
   });
 });
